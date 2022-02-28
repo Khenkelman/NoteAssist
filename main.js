1d4b4fc12f14a6a46e379da73da5fbbab0b0bb8c
Andrew <78057131+Womack-Git@users.noreply.github.com>
2022-02-28 04:00:25 +0000

    Add descriptive paragraph above search box, add placeholder text to search bar

--- main.js
+++ main.js
@@ -13,7 +13,52 @@ function categoryMove(command){
     categorySelected -= 1; 
   }
   var categoryCurrent = categories[categorySelected]; // < Very important to how this all works, but not a very good system I would say
-  document.getElementById('testp').innerHTML = categoryCurrent;
+  document.getElementById('categoryLarge').innerHTML = categoryCurrent;
+  document.getElementById('searchbox').placeholder = "Search " + categoryCurrent;
+  var categoryDescriptor = "";
+  switch(categoryCurrent){
+    case "Telehealth":
+      categoryDescriptor = "Is the client suitable for telehealth?";
+      break;
+    case "Presentation":
+      categoryDescriptor = "What was the client's presentation in the session?";
+      break;
+    case "Symptoms":
+      categoryDescriptor = "What symptoms has the client experienced since the last session?";
+      break;
+    case "Diagnoses":
+      categoryDescriptor = "These symptoms are consistent within ____ disorder.";
+      break;
+    case "Disruption":
+      categoryDescriptor = "How much do these symptoms disrupt the client's life?";
+      break;
+    case "Safety":
+      categoryDescriptor = "How would you describe the client's safety status?";
+      break;
+    case "Topics":
+      categoryDescriptor = "What general topics were discussed in the session?";
+      break;
+    case "Stage":
+      categoryDescriptor = "What is the current stage of therapy?";
+      break;
+    case "Interventions":
+      categoryDescriptor = "What are you doing (interventions) with the client to work towards client's goals?";
+      break;
+    case "Response":
+      categoryDescriptor = "How did the client receive the session?";
+      break;
+    case "Strengths":
+      categoryDescriptor = "What strengths did the client display in the session?";
+      break;
+    case "Progress":
+      categoryDescriptor = "How is the client's progress in relation to the treatment plan?";
+      break;
+    case "Tasks":
+      categoryDescriptor = "What steps will the therapist take in relationship to the treatment progress?";
+      break;
+  }
+  document.getElementById('descriptorParagraph').innerHTML = categoryDescriptor;
+  
   
   // Start of JSON calling function
   var xhttp = new XMLHttpRequest();
@@ -39,7 +84,7 @@ function categoryMove(command){
   // End of JSON calling function
 }
 
-// Right now this function literally just initializes the Telehealth titles when the page loads, because the above function relies on the arrows being clicked to be called. I am going to change this to onload DOM event and we can repurpose this function for other JSON related things, was more of a hotfix
+// Right now this function literally just initializes the Telehealth titles when the page loads, because the above function relies on the arrows being clicked to be called. I am going to change this and we can repurpose this function for other JSON related things, is more of a hotfix
 function loadOptions(command){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -54,7 +99,9 @@ function loadOptions(command){
           }
         }
         document.getElementById('boxes').innerHTML = output;
-        document.getElementById('testp').innerHTML = categoryCurrent;
+        document.getElementById('categoryLarge').innerHTML = categoryCurrent;
+        document.getElementById('searchbox').placeholder = "Search " + categoryCurrent;
+        document.getElementById('descriptorParagraph').innerHTML = "Is the client suitable for telehealth?";
       }
     }
   };
